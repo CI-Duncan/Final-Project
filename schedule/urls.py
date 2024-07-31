ed401d58ca141ad36f34c99885009d174f96ed2b
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-31 10:51:37 +0000

    test heroku dep

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('<int:year>/<str:month>/', views.home, name='home'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+import calendar
+from calendar import HTMLCalendar
 
-# Create your views here.
-class HomePage(TemplateView):
-    """
-    Displays Home Page
-    """
-    template_name = 'index.html'
+#Create your views here.
+def home(request, year, month):
+    mode = "Testing"
+    #Convert month to number
+    month_num = list(calendar.month_name).index(month)
+
+    return render(request,
+                'diary.html', {
+                    "mode": mode,
+                    "year": year,
+                    "month": month,
+                    "month_num": month_num,
+                })
